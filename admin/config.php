9d814ff0a18fc9216d8353651795f9fad36019a5
Dmitrii <d-zmeev@list.ru>
2023-03-24 23:03:30 +0500

    OsStore

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://bitrf.ru/admin/');
+define('HTTP_CATALOG', 'https://bitrf.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://bitrf.ru/admin/');
+define('HTTPS_CATALOG', 'https://bitrf.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/d/dzmeevwj/bitrf.ru/public_html/admin/');
+define('DIR_SYSTEM', '/home/d/dzmeevwj/bitrf.ru/public_html/system/');
+define('DIR_IMAGE', '/home/d/dzmeevwj/bitrf.ru/public_html/image/');
+define('DIR_LANGUAGE', '/home/d/dzmeevwj/bitrf.ru/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/d/dzmeevwj/bitrf.ru/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/d/dzmeevwj/bitrf.ru/public_html/system/config/');
+define('DIR_CACHE', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/d/dzmeevwj/bitrf.ru/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dzmeevwj_osstore');
+define('DB_PASSWORD', 'uVwUt4a&');
+define('DB_DATABASE', 'dzmeevwj_osstore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://bitrf.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://bitrf.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/d/dzmeevwj/bitrf.ru/public_html/catalog/');
+define('DIR_SYSTEM', '/home/d/dzmeevwj/bitrf.ru/public_html/system/');
+define('DIR_IMAGE', '/home/d/dzmeevwj/bitrf.ru/public_html/image/');
+define('DIR_LANGUAGE', '/home/d/dzmeevwj/bitrf.ru/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/d/dzmeevwj/bitrf.ru/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/d/dzmeevwj/bitrf.ru/public_html/system/config/');
+define('DIR_CACHE', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/d/dzmeevwj/bitrf.ru/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dzmeevwj_osstore');
+define('DB_PASSWORD', 'uVwUt4a&');
+define('DB_DATABASE', 'dzmeevwj_osstore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
